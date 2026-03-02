dd03b71067932e111b280c675a18f0ce2ef937b3
DarkGhoulLabs <parmarpriyanshu732@gmail.com>
2026-03-02 14:59:17 +0530

    Initial ORION project structure

---
+++ core/llm_interface.py
@@ -0,0 +1,16 @@
+import httpx
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
+MODEL_NAME = "mistral"
+
+def ask_orion(prompt: str)->str:
+    payload = {
+        "model": MODEL_NAME,
+        "prompt": prompt,
+        "stream": False
+    }
+
+    resposnse = httpx.post(OLLAMA_URL, json=payload, timeout=60)
+
+    result = resposnse.json()
+    return result["respnse"]
